f62756dec91aae2a3e8712d671a9f3b8162e1fb7
Mamanov Emil <emil.mamanov97@gmail.com>
2023-12-25 18:50:48 +0600

    added modal

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import Routes from './Routes';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
